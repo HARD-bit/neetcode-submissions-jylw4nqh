c4aa00b6f5a78038ede1a45989d7bafb156ce73c
Gabriele Rumi <rumi.gabriele99@gmail.com>
2026-04-21 09:06:03 +0200

    Update: sql-table-alias - submission-3

--- SQL/sql-table-alias/submission-3.sql
+++ SQL/sql-table-alias/submission-3.sql
@@ -1,31 +1,34 @@
-CREATE TABLE students (
-    id INT PRIMARY KEY,
-    name TEXT
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT
 );
 
-CREATE TABLE course_registrations (
-    id INT PRIMARY KEY,
-    student_id INT,
-    course_name TEXT
+CREATE TABLE employment_records (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    company_name TEXT,
+    job_title TEXT
 );
 
-INSERT INTO students (id, name) VALUES
-(1, 'Adam Smith'),
-(2, 'Thomas Jefferson'),
-(3, 'John Adams'),
-(4, 'James Madison');
 
-INSERT INTO course_registrations (id, student_id, course_name) VALUES
-(1, 1, 'Math'),
-(2, 1, 'Science'),
-(3, 2, 'Math'),
-(4, 2, 'History'),
-(5, 3, 'Science'),
-(6, 3, 'Math'),
-(7, 4, 'History'),
-(8, 4, 'Math');
+INSERT INTO users (id, name, email) VALUES
+(1, 'Alice', 'alice@gmail.com'),
+(2, 'Bob', 'bob@gmail.com'),
+(3, 'Charlie', 'charlie@gmail.com');
+
+INSERT INTO employment_records (id, user_id, company_name, job_title) VALUES
+(100, 1, 'Google', 'Software Engineer'),
+(300, 2, 'Microsoft', 'Data Scientist'),
+(500, 3, 'Amazon', 'Product Manager');
 -- Do not modify above this line. --
-SELECT students.name, course_name
-FROM students
-INNER JOIN course_registrations ON students.id = course_registrations.student_id
-ORDER BY students.name ASC;
+SELECT u.name, u.email,e.company_name,e.job_title
+FROM users AS u
+INNER JOIN employment_records AS e ON u.id = e.user_id
+ORDER BY e.company_name ASC;
+
+
+
+
+
+
